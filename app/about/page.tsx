bc37849b427c1a92aefefc19246d9ba350f6ddc0
Sikolia Malcolm <malcolmsikolia@gmail.com>
2025-05-06 23:07:30 +0300

    welllllloooo221212121

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -40,7 +40,7 @@ export default function About() {
           </div>
           {/* Add more team members as needed */}
           <div className="bg-gray-900 rounded-xl overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
-            <img src="/ceo.jpg" alt="Team Member" className="w-full h-64 object-cover" />
+            <img src="/ceo.JPG" alt="Team Member" className="w-full h-64 object-cover" />
             <div className="p-6">
               <h3 className="text-2xl font-semibold">Kijana E Kijaana</h3>
               <p className="text-gray-300">Co-Founder & Producer</p>
